9cfcde2ffbc9c7befb316785612151a6db649722
vdm-julieke <124454367+vdm-julieke@users.noreply.github.com>
2023-06-09 10:01:46 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -9,67 +9,72 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (IRwaarde == Vooruit) {
+    	
+    }
+    if (IRwaarde == Achteruit) {
+    	
+    }
+    if (IRwaarde == Rechts) {
+    	
+    }
+    if (IRwaarde == Links) {
+    	
+    }
+    if (IRwaarde == Stop) {
+    	
+    }
+})
+basic.forever(function () {
+    if (IRwaarde == Vooruit) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     }
     if (IRwaarde == Achteruit) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     }
     if (IRwaarde == Rechts) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         basic.pause(400)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     }
     if (IRwaarde == Links) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
         basic.pause(400)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     }
+})
+basic.forever(function () {
     if (IRwaarde == Stop) {
         maqueen.motorStop(maqueen.Motors.All)
-    }
-})
-basic.forever(function () {
-    if (IRwaarde == Vooruit) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    }
-    if (IRwaarde == Achteruit) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-    }
-    if (IRwaarde == Rechts) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-    }
-    if (IRwaarde == Links) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    }
-})
-basic.forever(function () {
-    if (IRwaarde == Stop) {
         basic.showLeds(`
             . . . . .
             . # # # .
